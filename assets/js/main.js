e771e6b5c3bf5899f235893b2b75064960950bfa
Clara Contreras <ccontrerassn@gmail.com>
2024-02-26 18:04:17 +0100

    Mejoras

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,20 +9,28 @@ const themeToggle = document.querySelector(".theme-toggle");
 const chosenTheme = window.localStorage && window.localStorage.getItem("theme");
 const chosenThemeIsDark = chosenTheme == "dark";
 const chosenThemeIsLight = chosenTheme == "light";
+const darkThemeBackgroundImage = "assets/images/logoNoName.png";
+const lightThemeBackgroundImage = "assets/images/logoLight.png";
+
 
 
 // Detect the color scheme the operating system prefers.
 function detectOSColorTheme() {
   if (chosenThemeIsDark) {
     document.documentElement.setAttribute("data-theme", "dark");
+    document.body.style.backgroundImage = `url('${darkThemeBackgroundImage}')`;
   } else if (chosenThemeIsLight) {
     document.documentElement.setAttribute("data-theme", "light");
+    document.body.style.backgroundImage = `url('${lightThemeBackgroundImage}')`;
   } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
     document.documentElement.setAttribute("data-theme", "dark");
+    document.body.style.backgroundImage = `url('${darkThemeBackgroundImage}')`;
   } else {
     document.documentElement.setAttribute("data-theme", "light");
+    document.body.style.backgroundImage = `url('${lightThemeBackgroundImage}')`;
   }
 }
+
 
 // Switch the theme.
 function switchTheme(e) {
